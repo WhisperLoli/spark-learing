495e29c8882ee63af6c8630ffba8eac86c2ff9a2
WhisperLoli <ahhunengjun@163.com>
2020-05-26 00:38:02 +0800

    meter demo

--- src/main/scala/com/spark/learning/metrics/MeterDemo.scala
+++ src/main/scala/com/spark/learning/metrics/MeterDemo.scala
@@ -1,28 +1,64 @@
 package com.spark.learning.metrics
 
-import java.util.concurrent.TimeUnit
-
-import com.codahale.metrics.{ConsoleReporter, Meter, MetricRegistry}
+import com.codahale.metrics.Meter
 
 /**
-  * meter demo
+  * meter demo：速率（已处理的请求总数除以进程已运行的秒数）
+  *
+  * 20-5-25 23:38:20 ===============================================================
+  * -- Meters ----------------------------------------------------------------------
+  * requests
+  * count = 1
+  * mean rate = 0.99 events/second
+  * 1-minute rate = 0.00 events/second
+  * 5-minute rate = 0.00 events/second
+  * 15-minute rate = 0.00 events/second
+  *
+  *
+  * 20-5-25 23:38:21 ===============================================================
+  * -- Meters ----------------------------------------------------------------------
+  * requests
+  * count = 1
+  * mean rate = 0.50 events/second
+  * 1-minute rate = 0.00 events/second
+  * 5-minute rate = 0.00 events/second
+  * 15-minute rate = 0.00 events/second
+  *
+  *
+  * 20-5-25 23:38:22 ===============================================================
+  * -- Meters ----------------------------------------------------------------------
+  * requests
+  * count = 1
+  * mean rate = 0.33 events/second
+  * 1-minute rate = 0.00 events/second
+  * 5-minute rate = 0.00 events/second
+  * 15-minute rate = 0.00 events/second
+  *
+  *
+  * 20-5-25 23:38:23 ===============================================================
+  * -- Meters ----------------------------------------------------------------------
+  * requests
+  * count = 1
+  * mean rate = 0.25 events/second
+  * 1-minute rate = 0.00 events/second
+  * 5-minute rate = 0.00 events/second
+  * 15-minute rate = 0.00 events/second
+  *
+  *
+  * 20-5-25 23:38:24 ===============================================================
+  * -- Meters ----------------------------------------------------------------------
+  * requests
+  * count = 1
+  * mean rate = 0.20 events/second
+  * 1-minute rate = 0.00 events/second
+  * 5-minute rate = 0.00 events/second
+  * 15-minute rate = 0.00 events/second
   */
 object MeterDemo {
   def main(args: Array[String]): Unit = {
-    startReport(metricRegistry)
+    startReport()
     val requests: Meter = metricRegistry.meter("requests")
     requests.mark()
-  }
-
-  def startReport(metricRegistry: MetricRegistry) {
-    val reporter = ConsoleReporter.forRegistry(metricRegistry)
-      .convertRatesTo(TimeUnit.SECONDS)
-      .convertDurationsTo(TimeUnit.MILLISECONDS)
-      .build()
-    reporter.start(1, TimeUnit.SECONDS)
-  }
-
-  def wait5Seconds() {
-    Thread.sleep(5*1000)
+    wait5Seconds()
   }
 }
