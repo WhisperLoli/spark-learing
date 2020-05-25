846b3305605a76c2ca6d96a76942450de09f6bbb
WhisperLoli <ahhunengjun@163.com>
2020-05-25 19:38:36 +0800

    future programming

---
+++ src/main/scala/com.spark.learning.future/FutureLearning.scala
@@ -0,0 +1,56 @@
+package com.spark.learning.future
+
+import scala.concurrent.{Await, Future, Promise}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.util.{Failure, Success}
+
+
+
+/**
+  * learning Future，启用多线程执行，顺序不能保证，如果主线程执行完，停止任务，可能子线程还没进行
+  */
+object FutureLearning {
+  def main(args: Array[String]): Unit = {
+    val result = Future{
+      Thread.sleep(10000)
+      1
+    }
+
+    // 最长等待9秒，线程睡眠10秒，等待超时，TimeoutException
+    // Await.result(result, 9 seconds)
+     Await.result(result, atMost = 10 seconds)
+    if (result.isCompleted) {
+      result.onComplete {
+        case Success(value) => println(s"current value is  $value")
+        case Failure(error) => println(error.getMessage)
+      }
+
+      result.onSuccess {
+        case value => println(value)
+      }
+    } else {
+      println("result is not finished")
+    }
+
+    // 案例二，与Promise一起使用
+    case class TaxCut(reduction: Int)
+
+    val taxCut = Promise[TaxCut]()
+
+    taxCut.success(TaxCut(20))
+
+    if (taxCut.future.isCompleted) {
+      taxCut.future.onComplete {
+        case Success(value) => println(s"current taxCut is $value")
+        case Failure(error) => println(error)
+      }
+    } else {
+      println("taxCut is not finished")
+    }
+
+    println(s"可用线程数 ${Runtime.getRuntime.availableProcessors()}")
+    Thread.sleep(10000)
+  }
+}
+
