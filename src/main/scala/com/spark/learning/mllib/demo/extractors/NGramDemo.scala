0d440f85de0fed72b4f7143ffdfdbf95a301c9a7
WhisperLoli <ahhunengjun@163.com>
2020-08-12 23:32:49 +0800

    n-gram

---
+++ src/main/scala/com/spark/learning/mllib/demo/extractors/NGramDemo.scala
@@ -0,0 +1,19 @@
+package com.spark.learning.mllib.demo.extractors
+import com.spark.learning.mllib.demo.spark
+import org.apache.spark.ml.feature.NGram
+
+/**
+  * 将n个单词按顺序组成一组的所有组合情况，Tokenizer用于分割句子
+  */
+object NGramDemo extends App {
+  val wordDataFrame = spark.createDataFrame(Seq(
+    (0, Array("Hi", "I", "heard", "about", "Spark")),
+    (1, Array("I", "wish", "Java", "could", "use", "case", "classes")),
+    (2, Array("Logistic", "regression", "models", "are", "neat"))
+  )).toDF("id", "words")
+
+  val ngram = new NGram().setN(3).setInputCol("words").setOutputCol("ngrams")
+
+  val ngramDataFrame = ngram.transform(wordDataFrame)
+  ngramDataFrame.select("ngrams").show(false)
+}
