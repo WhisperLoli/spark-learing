b7c72dca920921a088c081a7e9419ab4365df8db
WhisperLoli <ahhunengjun@163.com>
2020-08-14 13:35:24 +0800

    ALS checkpoint

--- src/main/scala/com/spark/learning/mllib/demo/recommend/ALSRecommend.scala
+++ src/main/scala/com/spark/learning/mllib/demo/recommend/ALSRecommend.scala
@@ -7,6 +7,9 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.{DoubleType, IntegerType, LongType}
 
 object ALSRecommend extends App {
+  spark.sparkContext.setLogLevel("WARN")
+  // 当血缘关系过长时，需要设置checkpoint,否则容易出现stackoverflow
+  spark.sparkContext.setCheckpointDir("checkpoint")
   import spark.implicits._
   val hotMovie = spark.read.csv("data/mllib/als/hot_movies.csv")
     .toDF("movieId", "rating", "movieName").cache()
@@ -38,7 +41,9 @@ object ALSRecommend extends App {
   })
 
   val df = userMovie.withColumn("userId", columnUserNameToId($"userName"))
-    .withColumn("userRating", when($"userRating" === "-1", 3.0d).otherwise($"userRating".cast(DoubleType)))
+    .withColumn("userRatingR", when($"userRating" === "-1", 3.0d).otherwise($"userRating".cast(DoubleType)))
+    .drop("userRating")
+    .withColumnRenamed("userRatingR", "userRating")
     .select($"movieId".cast(IntegerType), $"userId", $"userRating".cast(DoubleType))
     .cache()
 
@@ -47,18 +52,31 @@ object ALSRecommend extends App {
   val Array(training, test) = df.randomSplit(Array(0.8, 0.2))
 
   // Build the recommendation model using ALS on the training data
+  // 两种正则化方法L1和L2。
+  // L2正则化假设模型参数服从高斯分布，L2正则化函数比L1更光滑，所以更容易计算；
+  // L1假设模型参数服从拉普拉斯分布，L1正则化具备产生稀疏解的功能
   val als = new ALS()
     // iterations，迭代的次数
     .setMaxIter(20)
-    // 惩罚函数的因数，是ALS的正则化参数
+    // 惩罚函数的因数，是ALS的正则化参数，L2正则的系数
     .setRegParam(0.01)
     .setUserCol("userId")
     .setItemCol("movieId")
     .setRatingCol("userRating")
+    // 并行计算
+    .setNumBlocks(10)
+    // 表示原始User和Item的rating矩阵的值是否是评判的打分值，False表示是打分值，True表示是矩阵的值是某种偏好
+    .setImplicitPrefs(false)
+    // coldStartStrategy：String类型。有两个取值"nan" or "drop"。
+    // 这个参数指示用在prediction阶段时遇到未知或者新加入的user或item时的处理策略。
+    // 尤其是在交叉验证或者生产场景中，遇到没有在训练集中出现的user/item id时。
+    // "nan"表示对于未知id的prediction结果为NaN。
+    // "drop"表示对于transform()的入参DataFrame中出现未知ids的行，将会在包含prediction的返回DataFrame中被drop。
+    // 默认值是"nan"
+    .setColdStartStrategy("drop")
 
   val model = als.fit(training)
 
-  model.setColdStartStrategy("drop")
   val predictions = model.transform(test)
   predictions.show(false)
 
@@ -70,5 +88,7 @@ object ALSRecommend extends App {
   val rmse = evaluator.evaluate(predictions)
   println(s"Root-mean-square error = $rmse")
 
+  model.recommendForAllItems(10).show()
+  model.recommendForAllUsers(3).show()
 
 }
