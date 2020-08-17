be9e60de5289d04a85cdbd457334f571c6ae8332
WhisperLoli <ahhunengjun@163.com>
2020-08-17 13:53:54 +0800

    朴素贝叶斯

--- src/main/scala/com/spark/learning/mllib/demo/classification/NaiveBayesDemo.scala
+++ src/main/scala/com/spark/learning/mllib/demo/classification/NaiveBayesDemo.scala
@@ -5,6 +5,20 @@ import org.apache.spark.ml.classification.NaiveBayes
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 /**
   * 朴素贝叶斯
+  *
+  * 出门堵车的概率就是先验概率，P(堵车)
+  * 那么如果我们出门之前我们听到新闻说今天路上出了个交通事故，那么我们想算一下堵车的概率，这个就叫做条件概率，P(堵车|交通事故)
+  * 如果我们已经出了门，然后遇到了堵车，那么我们想算一下堵车时由交通事故引起的概率有多大，这是后验概率，也是一种条件概率 P(交通事故|堵车)
+  *
+  * 那这个就叫做后验概率
+  *
+  * 优点:
+  *   对小规模的数据表现很好，能个处理多分类任务
+  *   对缺失数据不太敏感，算法也比较简单，常用于文本分类
+  *
+  * 缺点:
+  *   需要知道先验概率，且先验概率很多时候取决于假设
+  *   特征之间需要相互独立
   */
 object NaiveBayesDemo extends App {
   spark.sparkContext.setLogLevel("WARN")
